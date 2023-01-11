c223ee83472f22ce56ac0edf775a040adb3b7b48
vicriesp <vicriesp@hotmail.com>
2023-01-10 19:24:23 -0700

    Count guesses

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int count = 0;
+        int guess = -1;
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            count = count +1;
+
+            if (guess > number)
+            
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higer");
+            }
+            else
+            {
+                Console.WriteLine("You Guessed it");
+                Console.WriteLine($"It took you {count} guesses");
+            }
+        }
     }
 }
