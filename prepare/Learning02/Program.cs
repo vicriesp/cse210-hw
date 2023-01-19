ae94523b71d967819dd2a3ca38a6464d69f73507
vicriesp <vicriesp@hotmail.com>
2023-01-18 20:49:39 -0700

    Learning02 done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Walmart";
+        job1._jobTitle = "Warehouse";
+        job1._startYear = 2003;
+        job1._endYear = 2009;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Cashier";
+        job2._company = "Smith's";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Victor Rivera";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
