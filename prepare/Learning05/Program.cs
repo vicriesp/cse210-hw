b5c6a33acb3eb62db57e41af25766267362645c0
vicriesp <vicriesp@hotmail.com>
2023-03-01 23:27:01 -0700

    Shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square area1 = new Square("Blue", 10);
+        shapes.Add(area1);
+
+        Rectangle area2 = new Rectangle("Green", 3, 6);
+        shapes.Add(area2);
+
+        Circle area3 = new Circle("Black", 2);
+        shapes.Add(area3);
+        
+        foreach (Shape  s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
